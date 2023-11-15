cb5ab346e218bb15767c72d4f0a0a43280c3d2cf
EvenEasy <hogopro15.kk@gmail.com>
2023-11-15 20:30:08 +0200

    :memo:

--- test.py
+++ test.py
@@ -1,45 +1,23 @@
-import logging
 import asyncio
 
 from aiober import Bot, Dispatcher
-from aiober.types import Message, Keyboard, KeyboardButton, ConversationStarted
-from aiober.filters import TextFilter, StateFilter
-from aiober.fsm.context import FSMcontext
+from aiober.types import Message
 
-
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(
-    'AUTH-TOKEN'
-)
-
+bot = Bot('AUTH-TOKEN')
 dp = Dispatcher(bot=bot)
 
-
-@dp.messages(TextFilter('test'))
-async def echo(message: Message, state: FSMcontext):
-    await state.set_state('test_state')
-    await state.update_data(name='pedik blyt')
-
-    await message.answer('new test))', Keyboard(Buttons=[KeyboardButton(Text='тест')]))
-    
-@dp.messages(StateFilter('test_state'))
-async def echo(message: Message, state: FSMcontext):
-    _state = await state.get_state()
-    _data = await state.get_data()
-
-    print(_state, _data)
-
-    await state.clear()
-
-    await message.copy_to(
-        message.sender.id,
-        text=f"{message.text} {_data.get('name')}"
-    )
+# router
+@dp.messages()
+async def echo(message: Message):
+    await message.copy_to(message.sender.id)
 
 
 async def main():
+
+    # start webhook
     await dp.start_webhook(
+        host='127.0.0.1',
+        port=8000,
         path='/viber/decorptest/update'
     )
 
